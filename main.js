8873c82ab0c4f67c3c82d8feba6160be2e074aed
t-coffe <tcoffe@edenschool.fr>
2023-10-27 15:52:25 +0200

    change_select

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+const select = document.getElementById("sortBy");
+const radioPrice = document.querySelector("#price");
+const radioName = document.querySelector("#name");
+const radioRelevance = document.querySelector("#relevance");
 
-let select = document.querySelector("#select");
-
-select.addEventListener("change", onChange);
-
-function onChange(){
-    console.log(select.value);
+function changeSelect() {
+  if (radioPrice.checked) {
+    select.value = "price";
+  } else if (radioName.checked) {
+    select.value = "name";
+  } else {
+    select.value = "relevance";
+  }
 }
+
+radioPrice.addEventListener("change", changeSelect);
+radioName.addEventListener("change", changeSelect);
+radioRelevance.addEventListener("change", changeSelect);
+
+function changeRadio() {
+  switch (select.value) {
+    case "name":
+      radioName.checked = true;
+      break;
+    case "relevance":
+      radioRelevance.checked = true;
+      break;
+    default:
+      radioPrice.checked = true;
+  }
+}
+
+select.addEventListener("change", changeRadio);
